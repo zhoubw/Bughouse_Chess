0be2d434a961545f1f9ce9332648814a03800df6
jasonluX13 <jasonluX13@gmail.com>
2015-06-01 12:20:57 -0400

    login

--- mongo.py
+++ mongo.py
@@ -1,9 +1,29 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient();
-db = client.manager;
-users = db.users;
-board = db.boards;
+client = MongoClient()
+db = client.manager
+users = db.users
+board = db.boards
 
 
+#Login Stuff
+
+def insert(username, password):
+    if (not user_exists(username)):
+        new_user = {
+            "username" : username,
+            "password" : password
+        }
+        users.insert(new_user)
+    else:
+        print("User exists")
+        
+def user_exists(username):
+    return users.find({"username" : username}).count() > 0
+
+
+
+
+
+#Game Stuff
